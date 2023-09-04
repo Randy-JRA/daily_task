ac3cab651e9ce0a8c1191dcad56791ce618272cd
Randy Ankiambom <jamrandyankiambom@gmail.com>
2023-09-04 08:34:43 +0100

    added home page view

---
+++ lib/HomePage/homePage.dart
@@ -0,0 +1,143 @@
+import 'package:daily_task/Components/gallerycard.dart';
+import 'package:flutter/material.dart';
+
+import '../DrawerPage/drawerPage.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  //here i would create a list of the pictures
+  List<String> urls = [
+    "https://www.freepik.com/free-vector/super-heroes-couple_4265873.htm#query=super%20heroes&position=11&from_view=search&track=ais",
+    "https://www.freepik.com/free-photo/superhero-kids_2861428.htm#query=super%20heroes&position=18&from_view=search&track=ais",
+    "https://www.freepik.com/premium-ai-image/super-hero-woman-with-african-american-appearance-is-super-hero-wearing-eye-maskant-generative-ai_60438049.htm#query=super%20heroes&position=27&from_view=search&track=ais",
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: const DrawerPage(),
+      appBar: AppBar(
+        elevation: 0.0,
+        title: const Text('Sprallery'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+                'Hello Your flutter Gallery',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 26.0,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const Text(
+              'Explore what you see',
+              style: TextStyle(
+                color: Colors.green,
+                fontSize: 20.0,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+            const SizedBox(height: 20,),
+
+            //here below is for the search field
+            Material(
+              elevation: 10.0,
+              borderRadius: BorderRadius.circular(30.0),
+              shadowColor: Colors.lightBlueAccent,
+              child: const TextField(
+                textAlign: TextAlign.start,
+                textAlignVertical: TextAlignVertical.center,
+                decoration: InputDecoration(
+                  hintText: 'Search tool',
+                  prefixIcon: Icon(Icons.search, color: Colors.black,size: 20,),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20,),
+
+            //here below is a tab bar
+            DefaultTabController(
+                length: 3,
+                child: Expanded(
+                  child: Column(
+                    children: [
+                      const TabBar(
+                        indicatorColor: Colors.blue,
+                        unselectedLabelColor: Colors.grey,
+                        labelColor: Colors.green,
+                        labelPadding: EdgeInsets.symmetric(horizontal: 8.0),
+                        tabs: [
+                          Tab(
+                            text: 'Trending',
+                          ),
+                          Tab(
+                            text: 'Promotions',
+                          ),
+                          Tab(
+                            text: 'Favorites',
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20.0,),
+                      Container(
+                        height: 300,
+                        child: TabBarView(
+                          children: [
+                            //below is our first tab view
+                            Container(
+                              child: ListView(
+                                scrollDirection: Axis.horizontal,
+                                children: [
+                                  //testing the card
+                                  galleryCard(urls[0], "Avengers", "New York", 2),
+                                  galleryCard(urls[1], "Avengers", "New York", 1),
+                                  galleryCard(urls[2], "Avengers", "New York", 3),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              child: ListView(
+                                scrollDirection: Axis.horizontal,
+                                children: [
+                                  galleryCard(urls[1], "Avengers", "New York", 1),
+                                  galleryCard(urls[2], "Avengers", "New York", 3),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              child: ListView(
+                                scrollDirection: Axis.horizontal,
+                                children: [
+                                  galleryCard(urls[1], "Avengers", "New York", 3),
+                                  galleryCard(urls[2], "Avengers", "New York", 1),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+
+                    ],
+                  ),
+                )
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'package:daily_task/HomePage/homePage.dart';
+
+
 //imports of the different pages
-import 'bottom navigation/homePage.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Sprallery',
-      home: HomePage(),
-    );
-  }
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
 }
