252e438d22ff3e04d32e9fbf1b8420eae8d61d2f
Randy Ankiambom <jamrandyankiambom@gmail.com>
2023-09-05 17:29:26 +0100

    added another change

--- lib/HomePage/homePage.dart
+++ lib/HomePage/homePage.dart
@@ -165,7 +165,7 @@ class _HomePageState extends State<HomePage> {
               ),
               GButton(
                 icon: Icons.home,
-                text: 'Hom',
+                text: 'Home',
               ),
               GButton(
                   icon: Icons.search,
